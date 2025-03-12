286812041eed2214b764255e8aed2105e3a4861c
조현준 <45157196+koboolean@users.noreply.github.com>
2025-03-12 12:13:25 +0900

    Commit 병렬성 처리

--- src/main/java/io/concurrency/chapter01/exam01/ConcurrencyExample.java
+++ src/main/java/io/concurrency/chapter01/exam01/ConcurrencyExample.java
@@ -3,11 +3,15 @@ package io.concurrency.chapter01.exam01;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * parallelStream()을 사용할 때는 적절한 데이터 크기와 CPU 개수를 고려하여 사용해야 최적의 성능을 낼 수 있다.
+ */
 public class ConcurrencyExample {
     public static void main(String[] args) {
 
+        // CPU Core 개수를 구한 후 구한 개수에 * 2를 수행하여 초과하도록 만든다.
         int cpuCores = Runtime.getRuntime().availableProcessors() * 2;
-//        int cpuCores = 13;
 
         // CPU 개수를 초과하는 데이터를 생성
         List<Integer> data = new ArrayList<>();
@@ -15,9 +19,12 @@ public class ConcurrencyExample {
             data.add(i);
         }
 
-        // CPU 개수를 초과하는 데이터를 병렬로 처리
-        long startTime2 = System.currentTimeMillis();
-        long sum2 = data.parallelStream()
+        long startTime = System.currentTimeMillis();
+
+        // CPU Core * 2의 데이터를 각 스레드에서 병렬처리하기 위해 사용한다.
+        // parallelStream()은 Collection 객체에 존재하는 함수로 ForkJoinPull 형식으로 작업이 이루어진다.
+        // 병렬성을 사용하지 않고 작업을 수행 시 stream() 형식으로 작업을 실행한다.
+        long sum = data.parallelStream()
                 .mapToLong(i -> {
                     try {
                         Thread.sleep(500);
@@ -28,9 +35,9 @@ public class ConcurrencyExample {
                 })
                 .sum();
 
-        long endTime2 = System.currentTimeMillis();
+        long endTime = System.currentTimeMillis();
 
-        System.out.println("CPU 개수를 초과하는 데이터를 병렬로 처리하는 데 걸린 시간: " + (endTime2 - startTime2) + "ms");
-        System.out.println("결과2: " + sum2);
+        System.out.println("CPU 개수를 초과하는 데이터를 병렬로 처리하는 데 걸린 시간: " + (endTime - startTime) + "ms");
+        System.out.println("결과 : " + sum);
     }
 }
--- src/main/java/io/concurrency/chapter01/exam01/ParallelismExample.java
+++ src/main/java/io/concurrency/chapter01/exam01/ParallelismExample.java
@@ -3,9 +3,13 @@ package io.concurrency.chapter01.exam01;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * parallelStream()을 사용할 때는 적절한 데이터 크기와 CPU 개수를 고려하여 사용해야 최적의 성능을 낼 수 있다.
+ */
 public class ParallelismExample {
     public static void main(String[] args) {
-//        int cpuCores = 1;
+
+        // CPU 개수만큼 작업을 수행한다.
         int cpuCores = Runtime.getRuntime().availableProcessors();
 
         // CPU 개수만큼 데이터를 생성
@@ -15,8 +19,12 @@ public class ParallelismExample {
         }
 
         // CPU 개수만큼 데이터를 병렬로 처리
-        long startTime1 = System.currentTimeMillis();
-        long sum1 = data.parallelStream()
+        long startTime = System.currentTimeMillis();
+
+        // CPU Core 만큼의 데이터를 각 스레드에서 병렬처리하기 위해 사용한다.
+        // parallelStream()은 Collection 객체에 존재하는 함수로 ForkJoinPull 형식으로 작업이 이루어진다.
+        // 병렬성을 사용하지 않고 작업을 수행 시 stream() 형식으로 작업을 실행한다.
+        long sum = data.parallelStream()
                 .mapToLong(i -> {
                     try {
                         Thread.sleep(500);
@@ -27,10 +35,10 @@ public class ParallelismExample {
                 })
                 .sum();
 
-        long endTime1 = System.currentTimeMillis();
+        long endTime = System.currentTimeMillis();
 
-        System.out.println("CPU 개수만큼 데이터를 병렬로 처리하는 데 걸린 시간: " + (endTime1 - startTime1) + "ms");
-        System.out.println("결과1: " + sum1);
+        System.out.println("CPU 개수만큼 데이터를 병렬로 처리하는 데 걸린 시간: " + (endTime - startTime) + "ms");
+        System.out.println("결과 : " + sum);
 
     }
 }
